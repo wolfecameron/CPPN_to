e68175c312216d0e9ba9f976a2e13c1a0f193cd2
Cameron Wolfe <wolfe.cameron@utexas.edu>
2017-09-10 11:14:48 -0500

    added relu, sin, and tanh activation functions to CPPNActivationFUnctions.py file

--- CPPNActivationFunctions.py
+++ CPPNActivationFunctions.py
@@ -1,7 +1,23 @@
-import math
 import numpy as np
 #This file will contain an extensive list of activation functions, which will then be kept as a property of each node. 
 
 #sigmoid activation function
 def sig(x):
     return (1/(1+np.exp(-x)))
+	
+	
+#ReLU activation function (linear if greater than 0, else 0)
+def relu(x):
+	return 0 if x<0 else x
+	
+
+#sin function (using numpy)	
+def sinAct(x):
+	return np.sin(x)
+	
+
+#tanh function (using numpy)
+def tanhAct(x):
+	return np.tanh(x)
+	
+	
