089cd0e9ef21cf51131ac42cc3dbf4a64c0451c4
Cameron Wolfe <wolfe.cameron@utexas.edu>
2017-09-11 13:12:46 -0500

    added empty activation function

--- CPPNActivationFunctions.py
+++ CPPNActivationFunctions.py
@@ -1,21 +1,23 @@
 import numpy as np
 #This file will contain an extensive list of activation functions, which will then be kept as a property of each node. 
 
+
+#no activation function (all nodes initialized with this)
+def noAct(x):
+	return x
+
 #sigmoid activation function
 def sig(x):
     return (1/(1+np.exp(-x)))
-	
-	
+		
 #ReLU activation function (linear if greater than 0, else 0)
 def relu(x):
 	return 0 if x<0 else x
 	
-
 #sin function (using numpy)	
 def sinAct(x):
 	return np.sin(x)
 	
-
 #tanh function (using numpy)
 def tanhAct(x):
 	return np.tanh(x)
